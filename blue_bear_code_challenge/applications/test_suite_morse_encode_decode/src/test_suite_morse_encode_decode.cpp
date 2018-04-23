d065587fd85e5045aa4c63ba8cd9203447cc0cf4
sundev.pabla <sundev.pabla@gmail.com>
2018-04-23 21:28:26 +0100

    Commiting blue bear code challenge

---
+++ blue_bear_code_challenge/applications/test_suite_morse_encode_decode/src/test_suite_morse_encode_decode.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test_suite_morse_encode_decode.cpp
+// Author      : Sundev Pabla
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
